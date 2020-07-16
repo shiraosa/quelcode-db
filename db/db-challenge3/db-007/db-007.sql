a778cd0e2c2e6ff36db06d1956cb605e092fce4e
shiraosa <tezukatrigger@gmail.com>
2020-07-16 16:45:33 +0900

    db-007を部分一致に修正

--- db/db-challenge3/db-007/db-007.sql
+++ db/db-challenge3/db-007/db-007.sql
@@ -2,6 +2,6 @@ SELECT id,room_name,overview,
 CASE is_sendable_file WHEN '0' THEN '禁止' WHEN '1' THEN '許可' END AS ファイル送信,
 is_direct_chat,is_deleted,created_at,created_by,updated_at,updated_by
 FROM chatrooms
-WHERE is_direct_chat = 1
+WHERE overview LIKE '%ダイレクトチャット'
 AND is_deleted = 0
 ORDER BY id ASC;
