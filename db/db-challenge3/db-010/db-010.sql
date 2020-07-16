f16cf808cd3fdab88138a59e308fbbafc39712da
shiraosa <tezukatrigger@gmail.com>
2020-07-16 17:00:53 +0900

    db-010修正。結合とwhere句の追加

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -7,8 +7,10 @@ JOIN (
     GROUP BY chats.chatroom_id
 )AS latest_chats
 ON chats.created_at = latest_chats.created_at
+AND chats.chatroom_id = latest_chats.chatroom_id
 JOIN `users`
 ON chats.created_by = users.id
 JOIN `chatrooms`
 ON latest_chats.chatroom_id = chatrooms.id
+WHERE users.is_deleted = 0
 ORDER BY latest_chats.chatroom_id ASC;
