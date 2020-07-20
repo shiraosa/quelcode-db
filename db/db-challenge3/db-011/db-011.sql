90b0622b32cb4ddb42903aac94e5edc4cb89a266
shiraosa <tezukatrigger@gmail.com>
2020-07-20 11:45:47 +0900

    スクリーンショット

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -2,6 +2,6 @@ BEGIN;
 UPDATE tasks
 SET is_completed = 1,updated_by = 1,updated_at = CURRENT_TIMESTAMP
 WHERE created_at >= '2020/4/26 09:00:00'
-AND created_at <=　'2020-04-26 11:30:00'
+AND created_at <= '2020-04-26 11:30:00'
 AND is_deleted = 0;
 COMMIT;
